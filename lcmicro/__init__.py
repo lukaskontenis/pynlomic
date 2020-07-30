6ef7f7253d1ad55a0d2382aedb3ecf8c59025d0a
lukaskontenis <lukas.kontenis@lightcon.com>
2020-07-30 09:57:42 +0300

    Removed UofT references

--- lcmicro/__init__.py
+++ lcmicro/__init__.py
@@ -6,9 +6,6 @@ A Python library for nonlinear microscopy and polarimetry.
 
 This is the library initialization file.
 
-Some ideas are taken from my collection of Matlab scripts developed while in
-the Barzda lab at the University of Toronto in 2011-2017.
-
 Copyright 2015-2020 Lukas Kontenis
 Contact: dse.ssd@gmail.com
 """
