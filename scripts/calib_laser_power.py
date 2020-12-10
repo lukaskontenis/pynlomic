3fcc7b28d4e84cd578eb99e48d589858fa3c8806
lukaskontenis <lukas.kontenis@lightcon.com>
2020-12-10 18:47:18 +0200

    Updating scripts and adding new for polarimetry

--- scripts/calib_laser_power.py
+++ scripts/calib_laser_power.py
@@ -1,3 +1,28 @@
+"""Calibrate laser power attenuator.
+
+Calculates calibrtion parameters for a rotating-waveplate laser power
+attenuator.
+
+This script is part of lcmicro, a Python library for nonlinear microscopy and
+polarimetry.
+
+Copyright 2015-2020 Lukas Kontenis
+Contact: dse.ssd@gmail.com
+"""
+# flake8: noqa
+# pylint: skip-file
+
+file_name = r"D:\Data\Maintenance\LCM1\Power calib\2020-09-17\calibration_2020_09_17.txt"
+
+print("=== lcmicro ===")
+print("Running laser power calibration script...")
+
+from lklib.util import handle_general_exception
 from lcmicro.report import calib_laser_power
 
-calib_laser_power(r"D:\Data\Maintenance\LCM1\Power calib\2020-09-17\calibration_2020_09_17.txt")
+try:
+    calib_laser_power(file_name)
+except Exception:
+    handle_general_exception("Could not perform calibration")
+
+input("Press any key to close this window...")
