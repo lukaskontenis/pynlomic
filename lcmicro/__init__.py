087a6d79f6bbdb75bb74f595a4c5a798f3d6ec5e
Lukas Kontenis <dse.ssd@gmail.com>
2021-02-07 19:18:20 +0200

    polarimetry dataio submodule added to init scripts

--- lcmicro/__init__.py
+++ lcmicro/__init__.py
@@ -9,4 +9,8 @@ This is the library initialization file.
 Copyright 2015-2020 Lukas Kontenis
 Contact: dse.ssd@gmail.com
 """
-from . import polarimetry
+# flake8: noqa
+
+from lcmicro.dataio import *
+
+from lcmicro import polarimetry
