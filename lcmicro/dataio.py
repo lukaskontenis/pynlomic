e03248201e46eff6f227417f93e882e208b7e10f
Lukas Kontenis <dse.ssd@gmail.com>
2021-02-15 13:29:31 +0200

    New function get_psf_data_file_name

--- lcmicro/dataio.py
+++ lcmicro/dataio.py
@@ -1,9 +1,9 @@
-"""Microscopy data input/output functions
+"""Microscopy data input/output functions.
 
 This module is part of lcmicro, a Python library for nonlinear microscopy and
 polarimetry.
 
-Copyright 2015-2020 Lukas Kontenis
+Copyright 2015-2021 Lukas Kontenis
 Contact: dse.ssd@gmail.com
 """
 import os
@@ -11,6 +11,12 @@ from lklib.fileread import list_files_with_extension
 
 
 def get_microscopy_data_file_name(file_name=None):
+    """Get microscopy data file name in the current dir.
+
+    This function returns the name of a microscopy data file, if there is only
+    one in the current directory. It does that by listing .dat files in the
+    current directory while skipping the PolStates.dat file.
+    """
     file_names = list_files_with_extension(ext='dat')
 
     # Remove PolStates.dat files
@@ -29,4 +35,25 @@ def get_microscopy_data_file_name(file_name=None):
         return file_name
     else:
         print("More than one dat file found, specify which to load")
+        return None
+
+
+def get_psf_data_file_name(file_name=None):
+    """Get PSF data file name in the current dir.
+
+    This function returns the name of a PSF data file, if there is only
+    one in the current directory. It does that by listing .txt files in the
+    current directory.
+    """
+    file_names = list_files_with_extension(ext='txt')
+
+    if len(file_names) == 0:
+        print("No data files found")
+        return None
+    if len(file_names) == 1:
+        file_name = file_names[0]
+        print("Found a single TXT file '{:s}s', loading it".format(file_name))
+        return file_name
+    else:
+        print("More than one TXT file found, specify which to load")
         return None
