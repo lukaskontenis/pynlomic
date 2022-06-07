7aaadd4a2f4c4165ec310d4771e5a7612cf57fa6
Lukas Kontenis <dse.ssd@gmail.com>
2022-06-07 19:52:30 +0300

    Fixing dependencies

--- setup.py
+++ setup.py
@@ -18,7 +18,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        'lklib>=0.0.15'
+        'numpy>=1.19.5', 'matplotlib>=3.3.3', 'scipy>=1.5.4',
+        'tifffile>=2020.9.3', 'opencv-python>=4.4.0.46', 'lkfit>=0.1.0',
+        'lkcom>=0.2.0'
     ],
     python_requires='>=3.6',
     data_files=[
@@ -26,7 +28,6 @@ setuptools.setup(
         'scripts/calib_laser_power.py',
         'scripts/gen_img_report.py',
         'scripts/lcmicro_to_png_tiff.py',
-        'scripts/make_nsmp_tiff.py',
         'scripts/make_pipo_tiff_piponator.py',
         'scripts/make_psf_figure.py',
         'scripts/tiff_to_png.py'])],
