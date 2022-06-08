af6a895b4046b71287163d2f2dfd66f87c22d425
Lukas Kontenis <dse.ssd@gmail.com>
2022-06-08 21:44:08 +0300

    Release v0.9.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pynolmic",
-    version="0.9.2",
+    version="0.9.3",
     author="Lukas Kontenis",
     author_email="dse.ssd@gmail.com",
     description="A Python library for nonlinear microscopy.",
